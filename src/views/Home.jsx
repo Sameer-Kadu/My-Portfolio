b4f28aeeeb3d29d36f20d83c96ed1860d07921f4
Sameer Kadu <Samirkadu8@gmail.com>
2024-05-31 12:47:39 +0530

    resume update

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -71,7 +71,8 @@ const Home = () => {
             </div>
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div className="mt-3 sm:mt-0 cursor-pointer w-1/2"> 
-                <a href="/Sameer-Kadu-Resume.pdf"
+                <a href="https://drive.google.com/file/d/1XdSEdKHYEuwkZNk3QsUuuBc9pFBUenvm/view" 
+                target="_blank"
                 className="w-full  items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10"
                  
                  >
